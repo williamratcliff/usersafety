6dabdea3e6d5ed749ceb3da08d8acfbd4ef91892
Patrick <mantrick@gmail.com>
2013-05-09 23:31:13 -0400

    new model

--- safetylist/models.py
+++ safetylist/models.py
@@ -1,38 +1,40 @@
 from django.db import models
 from django.forms import ModelForm
+from django.contrib.localflavor.us.models import PhoneNumberField
+
 # Create your models here.
 
 class Checklist(models.Model):
 		name		   = models.CharField (max_length =255, blank = False)
-		phone		   = models.IntegerField()
+		phone		   = models.IntegerField() # PhoneNumberField()
 		emerg_contact  = models.CharField (max_length =255, blank = False)
 		relationship   = models.CharField (max_length =255, blank = False)
 		home_inst	   = models.CharField (max_length =255, blank = False)
 		nist_contact   = models.CharField (max_length =255, blank = False)
 		nist_div	   = models.CharField (max_length =255, blank = False)
-		nist_ext	   = models.IntegerField()
+		nist_ext	   = models.IntegerField() # custom validate data for 4 digit
 
 		
-		emerg_num	 = models.CharField (max_length =2, blank=False)
-		emerg_action = models.CharField (max_length =2, blank=False)
-		accid_rep 	 = models.CharField (max_length =2, blank=False)
-		gen_lab 	 = models.CharField (max_length =2, blank=False)
-		saf_info 	 = models.CharField (max_length =2, blank=False)
-		protective	 = models.CharField (max_length =2, blank=False)
-		hazacomm 	 = models.CharField (max_length =2, blank=False)
-		comp_reg 	 = models.CharField (max_length =2, blank=False)
-		inst_op 	 = models.CharField (max_length =2, blank=False)
-		crane_saf 	 = models.CharField (max_length =2, blank=False)
-		liq_handle 	 = models.CharField (max_length =2, blank=False)
-		gen_acid 	 = models.CharField (max_length =2, blank=False)
-		hyd_acid 	 = models.CharField (max_length =2, blank=False)
-		glove_box 	 = models.CharField (max_length =2, blank=False)
-		x_ray 	     = models.CharField (max_length =2, blank=False)
-		lab_torch 	 = models.CharField (max_length =2, blank=False)
-		spin_coater  = models.CharField (max_length =2, blank=False)
-		furnace 	 = models.CharField (max_length =2, blank=False)
-		lab_press 	 = models.CharField (max_length =2, blank=False)
-		other 	     = models.CharField (max_length =2, blank=False)
+		emerg_num	 = models.BooleanField()
+		emerg_action = models.BooleanField()
+		accid_rep 	 = models.BooleanField()
+		gen_lab 	 = models.BooleanField()
+		saf_info 	 = models.BooleanField()
+		protective	 = models.BooleanField()
+		hazacomm 	 = models.BooleanField()
+		comp_reg 	 = models.BooleanField()
+		inst_op 	 = models.BooleanField()
+		crane_saf 	 = models.BooleanField()
+		liq_handle 	 = models.BooleanField()
+		gen_acid 	 = models.BooleanField()
+		hyd_acid 	 = models.BooleanField()
+		glove_box 	 = models.BooleanField()
+		x_ray 	     = models.BooleanField()
+		lab_torch 	 = models.BooleanField()
+		spin_coater  = models.BooleanField()
+		furnace 	 = models.BooleanField()
+		lab_press 	 = models.BooleanField()
+		other 	     = models.BooleanField()
 #		other_text	 = models.CharField (max_length =255, blank=True null=True)
 
 #		def	__str__(self):
@@ -42,3 +44,5 @@ class Checklist(models.Model):
 class ChecklistForm (ModelForm):
 	class Meta:
 		model = Checklist
+		
+		#reportlab to generate the PDF file
