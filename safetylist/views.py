ee20ddcda92e35eb244df8f4713e1a37f2845f29
Patrick <mantrick@gmail.com>
2013-05-09 23:41:40 -0400

    Revised View

--- safetylist/views.py
+++ safetylist/views.py
@@ -3,15 +3,39 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+import os,sys
+from subprocess import Popen, PIPE
+import hashlib
 
 def addcontact(request):
-	form = ChecklistForm(request.POST)
-	if form.is_valid():
-		form.save()
-		return HttpResponseRedirect('/list/')
-#	else:
-#		formset = ContactForm ()
-	return render(request,"awlist.html", {"form":form})
+	if request.method=='POST':
+		form = ChecklistForm(request.POST)
+		if 1:
+			t=render(request,"checklist3.html", {"form":form})
+			temp_html=t.content
+			myfilebase=hashlib.sha1(temp_html).hexdigest()
+			myfilename_html=os.path.join(r'/tmp',myfilebase+'.html')
+			myfilename_pdf=os.path.join(r'/tmp',myfilebase+'.pdf')
+			myfile=open(myfilename_html,'w')
+			myfile.write(temp_html)
+			myfile.close()	
+			process=Popen(["wkhtmltopdf", 'file://'+myfilename_html, myfilename_pdf])
+			#process=Popen(["wkhtmltopdf", "--disable-javascript", 'file://'+myfilename_html, myfilename_pdf])			
+			print "done"		
+		if form.is_valid():
+			
+			#form.save()
+			return HttpResponseRedirect('/list/')
+	else:
+		form= ChecklistForm()
+		if 0:
+			t=render(request,"awlist.html", {"form":form})
+			myfile=open(r'/tmp/outfile.html','w')
+			myfile.write(t.content)
+			myfile.close()	
+			
+			
+	return render(request,"checklist3.html", {"form":form})
 	
 def listcontact(request):
 	contact = Checklist.objects.all()
